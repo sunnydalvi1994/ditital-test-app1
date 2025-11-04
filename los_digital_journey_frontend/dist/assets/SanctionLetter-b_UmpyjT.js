6f3e1facc8c7e64dfd8efd630dc910b775339587
sunnydalvi1994 <sunny.dalvi@kalolytic.com>
2025-11-04 16:58:36 +0530

    Delete los_digital_journey_frontend directory

--- los_digital_journey_frontend/dist/assets/SanctionLetter-b_UmpyjT.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-CN90tRrT.js";function n(){return t.jsx("div",{children:"SanctionLetter"})}export{n as default};
--- los_digital_journey_frontend/src/pages/SanctionLetter.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function SanctionLetter() {
-  return (
-    <div>SanctionLetter</div>
-  )
-}
-
-export default SanctionLetter
